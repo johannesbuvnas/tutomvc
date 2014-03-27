43f99e1d7707bc648b023d6572b41e94e82a89cc
Johannes Buvnäs <johannes.buvnas@ps-communication.se>
2014-03-27 16:39:57 +0100

    Appended cache support to Proxy

--- src/php/core/model/proxy/Proxy.php
+++ src/php/core/model/proxy/Proxy.php
@@ -5,8 +5,9 @@ class Proxy extends CoreClass
 {
 	/* PROTECTED VARS */
 	protected $_map = array();
-
 	protected $_name;
+	protected $_cacheEnabled = FALSE;
+	protected $_cacheExpirationTimeInSeconds = 0;
 
 	function __construct( $name = NULL )
 	{
@@ -15,27 +16,37 @@ class Proxy extends CoreClass
 
 	public function add( $item, $key = NULL )
 	{
-		if( is_null( $key ) )
+		$key = is_null( $key ) ? uniqid() : $key;
+
+		if(!$this->has( $key ))
 		{
-			$this->_map[] = $item;
+			$this->_map[ $key ] = $item;
+
+			if( $this->getCacheEnabled() )
+			{
+				wp_cache_set( $key, $item, $this->getCacheGroupName(), $this->_cacheExpirationTimeInSeconds );
+			}
+
 			return $item;
 		}
 		else
 		{
-			if(!$this->has( $key ))
-			{
-				$this->_map[ $key ] = $item;
-				return $item;
-			}
-			else
-			{
-				return $this->get( $key );
-			}
+			return $this->get( $key );
 		}
+	}
+
+	public function delete( $key )
+	{
+		if(array_key_exists( $key, $this->_map )) unset( $this->_map[ $key ] );
+		if($this->getCacheEnabled()) wp_cache_delete( $key, $this->getCacheGroupName() );
+
+		return $this;
 	}
 
 	public function has( $key )
 	{
+		if( $this->getCacheEnabled() && wp_cache_get( $key, $this->getCacheGroupName() ) !== FALSE ) $this->_map[ $key ] = wp_cache_get( $key, $this->getCacheGroupName() );
+
 		return array_key_exists( $key, $this->_map );
 	}
 
@@ -52,5 +63,24 @@ class Proxy extends CoreClass
 	public function getName()
 	{
 		return $this->_name;
+	}
+
+	/**
+	*	Use WP Cache to cache all items added to this proxy.
+	*/
+	public function setCacheEnabled( $value, $cacheExpirationTimeInSeconds = 0 )
+	{
+		$this->_cacheEnabled = $value;
+		$this->_cacheExpirationTimeInSeconds;
+
+		return $this;
+	}
+	public function getCacheEnabled()
+	{
+		return $this->_cacheEnabled;
+	}
+	public function getCacheGroupName()
+	{
+		return $this->getFacade()->getKey() . "/model/proxy/" . $this->getName();
 	}
 }
