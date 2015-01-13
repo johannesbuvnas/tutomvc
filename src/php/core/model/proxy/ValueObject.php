af3b34b9a36dfed8b590e8db6492c6bb2c727f14
Johannes Buvnäs <johannes@stickerapp.se>
2015-01-13 15:21:41 +0100

    ValueObject divided into NameObject

--- src/php/core/model/proxy/ValueObject.php
+++ src/php/core/model/proxy/ValueObject.php
@@ -1,40 +1,25 @@
 <?php
-namespace tutomvc;
+	namespace tutomvc;
 
-class ValueObject implements IValueObject
-{
-	/* VARS */
-	private $_name;
-	private $_value;
+	class ValueObject extends NameObject
+	{
+		/* VARS */
+		protected $_value;
 
+		function __construct( $name, $value )
+		{
+			parent::__construct( $name );
+			$this->setValue( $value );
+		}
 
-	function __construct( $name, $value )
-	{
-		$this->setName( $name );
-		$this->setValue( $value );
-	}
+		/* SET AND GET */
+		public function setValue( $value )
+		{
+			$this->_value = $value;
+		}
 
-	/* SET AND GET */
-	public function setValue( $value )
-	{
-		$this->_value = $value;
+		public function getValue()
+		{
+			return $this->_value;
+		}
 	}
-	public function getValue()
-	{
-		return $this->_value;
-	}
-	function setName( $name )
-	{
-		$this->_name = $name;
-	}
-	function getName()
-	{
-		return $this->_name;
-	}
-}
-
-interface IValueObject
-{
-	function setName( $name );
-	function getName();
-}
