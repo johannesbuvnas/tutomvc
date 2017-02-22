55958118a33ebb7cc5e11fa3a69690768ea7df2f
Johannes Buvnäs <johannes@stickerapp.se>
2017-02-22 13:32:34 +0100

    no message

--- src/php/wp/TutoMVC.php
+++ src/php/wp/TutoMVC.php
@@ -44,7 +44,7 @@
 
 				// Figure out URL to this plugin
 				self::$_documentRoot = str_replace( $_SERVER[ 'SCRIPT_NAME' ], '', $_SERVER[ 'SCRIPT_FILENAME' ] );
-				self::$_url          = $wpURL . FileUtil::filterFileReference( substr( self::$_root, strripos( self::$_root, self::$_documentRoot ) + strlen( self::$_wpRelativeRoot ) + strlen( self::$_documentRoot ) ) );
+				self::$_url          = $wpURL . FileUtil::sanitizePath( substr( self::$_root, strripos( self::$_root, self::$_documentRoot ) + strlen( self::$_wpRelativeRoot ) + strlen( self::$_documentRoot ) ) );
 
 				self::$_initiated = TRUE;
 
