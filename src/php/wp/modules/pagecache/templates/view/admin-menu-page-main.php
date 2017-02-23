af285efbd311a3be0fbf8b3885afeb65dd7b6f38
Johannes Buvnäs <johannes@stickerapp.se>
2017-02-23 11:19:48 +0100

    no message

--- src/php/wp/modules/pagecache/templates/view/admin-menu-page-main.php
+++ src/php/wp/modules/pagecache/templates/view/admin-menu-page-main.php
@@ -59,7 +59,6 @@
 							<?php
 								foreach ( $indexedFiles as $key => $uri )
 								{
-									$created = date( "Y-m-d H:i:s", filemtime( $key ) );
 									?>
                                     <li class="list-group-item">
                                         <a href="<?php echo "http://$uri"; ?>" target="_blank">
