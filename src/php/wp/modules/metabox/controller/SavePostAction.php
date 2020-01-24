2c6ef9c8a5e0e7fac98be08aafd1ebb9f159e5f9
Johannes Buvnäs <johannes@stickerapp.com>
2020-01-24 15:42:50 +0100

    Since we're parsing metaboxes now - we need to identify when they are deleted

--- src/php/wp/modules/metabox/controller/SavePostAction.php
+++ src/php/wp/modules/metabox/controller/SavePostAction.php
@@ -9,6 +9,10 @@
 	namespace tutomvc\wp\metabox;
 
 	use tutomvc\wp\core\controller\command\ActionCommand;
+	use function array_key_exists;
+	use function func_get_arg;
+	use function get_post_type;
+	use function in_array;
 
 	class SavePostAction extends ActionCommand
 	{
@@ -19,10 +23,18 @@
 			/** @var MetaBox $metaBox */
 			foreach ( MetaBoxModule::getProxy()->getMap() as $metaBox )
 			{
-				if ( !empty($_POST) && in_array( get_post_type( $postID ), $metaBox->getPostTypes() ) && isset($_POST[ $metaBox->getName() ]) )
+				if ( !empty( $_POST ) && in_array( get_post_type( $postID ), $metaBox->getPostTypes() ) && isset( $_POST[ $metaBox->getName() ] ) )
 				{
-					if ( $metaBox->parse( $_POST ) )
+					if ( array_key_exists( $metaBox->getName(), $_POST ) )
 					{
+						if ( !empty( $_POST[ $metaBox->getName() ] ) )
+						{
+							$metaBox->parse( $_POST );
+						}
+						else
+						{
+							$metaBox->setFissions( 0 );
+						}
 						$metaBox->update( $postID );
 					}
 					//TODO: Add admin notification if metabox contains errors
